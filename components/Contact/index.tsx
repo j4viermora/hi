25504327ccbcf4492cd87996add0cdb3dd2818d1
Javier Mora <j4viermora@MacBook-Air-de-Javier.local>
2021-09-02 22:01:03 -0400

    add form action

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, {  SyntheticEvent, useState } from 'react';
 
 export const Contact = () => {
+
+	const emailTo = 'javier@expressativo.com';
+
+	const [form, setForm] = useState({
+		name:'',
+		email:'',
+		subject:'',
+		message:''
+	});
+
+	const { name, email, subject, message} = form;
+
+	const finallyMessage = `Hola mi nombre es ${name} \n Mi correo es: ${email} \n ${message}`
+
+	const handleChange = ({target}:any)=>{
+		setForm({
+			...form,
+			[target.id] : target.value
+		})
+	};
+
+	const resetForm = () => {
+		setForm({
+			...form,
+			email: '',
+			name: '',
+			subject: '',
+			message: '',
+		})
+	}
+
+	const handleSubmit = (event:SyntheticEvent) => {
+		event.preventDefault()
+		console.log({name, email, subject, message})
+		document.getElementById('emailTo')?.click()
+	};
+
+
 	return (
 		<section className='section is-medium' id='contact'>
 			<h3 className='title'>Contact</h3>
@@ -9,6 +47,7 @@ export const Contact = () => {
 				to contact me
 			</p>
 			<form
+				onSubmit={handleSubmit}
 				className='mt-5'
 				style={{
 					maxWidth: ' 500px',
@@ -24,6 +63,8 @@ export const Contact = () => {
 							type='text'
 							required
 							placeholder='Pedro Tapia'
+							value={name}
+							onChange={handleChange}
 						/>
 					</div>
 				</div>
@@ -34,26 +75,29 @@ export const Contact = () => {
 					</label>
 					<div className='control'>
 						<input
-							id='name'
+							id='email'
 							className='input'
 							type='email'
 							placeholder='pedro@cuidadgotica.com'
 							required
+							value={email}
+							onChange={handleChange}
 						/>
 					</div>
 				</div>
-
 				<div className='field'>
 					<label htmlFor='name' className='label'>
 						Subject
 					</label>
 					<div className='control'>
 						<input
-							id='name'
+							id='subject'
 							className='input'
 							type='text'
 							placeholder='Future of gothic city without batman'
 							required
+							value={subject}
+							onChange={handleChange}
 						/>
 					</div>
 				</div>
@@ -61,12 +105,18 @@ export const Contact = () => {
 					<label className='label'>Message</label>
 					<div className='control'>
 						<textarea
+							id='message'
 							className='textarea'
-							placeholder='Hi Javier, since Batman has been missing Gordon for a week and I thought it was good to ask for your help'></textarea>
+							placeholder='Hi Javier, since Batman has been missing Gordon for a week and I thought it was good to ask for your help'
+							value={message}
+							onChange={handleChange}
+							>
+							</textarea>
 					</div>
 				</div>
 
 				<button className='button'>Send message</button>
+				<a id='emailTo' href={`mailto:${emailTo}?subject=${subject}&body=${finallyMessage}`}></a>
 			</form>
 		</section>
 	);
