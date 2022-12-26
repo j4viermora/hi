16eb5ccf358262872b18c25448d196469382789c
Javier A Mora gil <55900303+j4viermora@users.noreply.github.com>
2022-12-26 16:52:19 -0600

    Update some texts

--- components/Experience/Experience.tsx
+++ components/Experience/Experience.tsx
@@ -17,7 +17,7 @@ export const Experience = () => {
 					</li>
 					<li>
 						<h3 className='title is-5 mt-6'>
-							Aras gruop - frontend dev (profesional independiente) agosto 2021 - febrero 2022
+							Aras group - frontend dev (profesional independiente) agosto 2021 - febrero 2022
 						</h3>
 						<p>
 						Desarrollo de su plataforma interna para la gestion de portafolio de inversión. 
@@ -30,13 +30,7 @@ export const Experience = () => {
 						</h3>
 						<p>
 							Diseño e implementación de sitios webs, construidos
-							con wordpress y estaticos, puedes ver mi perfil de
-							workana{' '}
-							<a
-								href='https://www.workana.com/freelancer/62d3e6273bc3792e5100c6e539b5a1bd'
-								target='_blank'>
-								aquí
-							</a>
+							con Wordpress y estáticos
 						</p>
 					</li>
 				</ul>
