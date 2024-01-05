5ec114450aee2a48fe3111a3d2a3c4329857ab33
Javier A Mora gil <j4viermora@gmail.com>
2024-01-05 00:59:38 -0400

    some changes

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,25 +1,21 @@
-import React, { FC, useEffect, useState } from 'react';
-import { BlogLayout } from '../../layout/BlogLayout';
-import Head from 'next/head';
+import React, { FC, useEffect, useState } from "react";
+import { BlogLayout } from "../../layout/BlogLayout";
+import Head from "next/head";
 
-import { getAllFilesMetadata } from '../../lib/mdx';
-import { ListItemPostBlog } from '../../components/ListItemPostBlog';
-import { TitlePost } from '../../interfaces/interfaces';
-import { useRouter } from 'next/router';
+import { getAllFilesMetadata } from "../../lib/mdx";
+import { ListItemPostBlog } from "../../components/ListItemPostBlog";
+import { TitlePost } from "../../interfaces/interfaces";
+import { useRouter } from "next/router";
 
 interface Props {
   posts: TitlePost[];
 }
 
-const blog: FC<Props> = ({ posts }) => {
+const Blog: FC<Props> = ({ posts }) => {
   const { query } = useRouter();
   const [listPostMetadata, updateListMetadata] = useState(posts);
   const [hasResutl, setHasResult] = useState(true);
   const [isSearchResult, setIsSearchResult] = useState(false);
-
-  useEffect(() => {
-    query.q && filterPosts();
-  }, [query]);
 
   const filterPosts = () => {
     const filteredPostMetada = posts.filter((postMetadata) =>
@@ -38,24 +34,28 @@ const blog: FC<Props> = ({ posts }) => {
     setIsSearchResult(false);
   };
 
+  useEffect(() => {
+    query.q && filterPosts();
+  }, [query, filterPosts]);
+
   return (
     <>
       <Head>
         <title>Blog</title>
         <meta
-          name='description'
-          content='Blog sobre desarrollo web y tecnología by Javier Mora'
+          name="description"
+          content="Blog sobre desarrollo web y tecnología by Javier Mora"
         />
         <meta
-          name='keywords'
-          content='Blog desarrollo web, Desarrollo web, JavaScript tips'
+          name="keywords"
+          content="Blog desarrollo web, Desarrollo web, JavaScript tips"
         />
-        <meta name='robots' content='index' />
+        <meta name="robots" content="index" />
       </Head>
       <BlogLayout>
-        <section className='section is-medium mt-5'>
-          <h2 className='title mb-6'>
-            {isSearchResult ? 'Resultado de búsqueda' : 'Últimos Artículos:'}
+        <section className="section is-medium mt-5">
+          <h2 className="title mb-6">
+            {isSearchResult ? "Resultado de búsqueda" : "Últimos Artículos:"}
           </h2>
           {hasResutl ? (
             <ul>
@@ -66,7 +66,7 @@ const blog: FC<Props> = ({ posts }) => {
           ) : (
             <>
               <h2>No hay resultados para tu busqueda...</h2>
-              <button className='button mt-5' onClick={resetArticlesList}>
+              <button className="button mt-5" onClick={resetArticlesList}>
                 Ver todos los artículos
               </button>
             </>
@@ -77,7 +77,7 @@ const blog: FC<Props> = ({ posts }) => {
   );
 };
 
-export default blog;
+export default Blog;
 
 export const getStaticProps = async () => {
   const posts = await getAllFilesMetadata();
